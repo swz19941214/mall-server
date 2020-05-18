3ec43e6c8ce499fed700259a385a1505b8852a80
shiweizhi <283707645@qq.com>
2020-05-18 20:58:51 +0800

    搭建maven项目 构建mall-tiny模块

---
+++ mall-tiny/src/test/java/com/mall/tiny/MallTinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.tiny;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTinyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
